2e71e0798456422c43d3b607d86787d7cca8610d
Rahul Kumar <33459576+RahulkumarRV@users.noreply.github.com>
2021-05-21 13:32:28 +0530

    Update find_dublicate.cpp

--- find_dublicate.cpp
+++ find_dublicate.cpp
@@ -22,3 +22,20 @@ int findDuplicate(vector<int>& nums) {
     }
     return -1;
 }
+
+//other way
+int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+    }
