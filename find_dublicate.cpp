af19081264b7487fa198786de7ec5db723807b07
Rahul Kumar <33459576+RahulkumarRV@users.noreply.github.com>
2021-05-21 13:28:52 +0530

    Update find_dublicate.cpp

--- find_dublicate.cpp
+++ find_dublicate.cpp
@@ -10,3 +10,15 @@ public:
         return -1;
     }
 };
+
+//other way 
+int findDuplicate(vector<int>& nums) {
+    set<int> s;
+    for(int i=0; i<nums.size(); i++){
+        if(s.find(nums[i]) != s.end()){
+            return nums[i];
+        }
+        s.insert(nums[i]);
+    }
+    return -1;
+}
