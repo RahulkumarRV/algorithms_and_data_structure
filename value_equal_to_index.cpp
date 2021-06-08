c4558a4d9a51f9cee0f33ed12cb5a74ba3c30b90
Rahul Kumar <33459576+RahulkumarRV@users.noreply.github.com>
2021-06-08 14:46:29 +0530

    value equal to its index

---
+++ value_equal_to_index.cpp
@@ -0,0 +1,9 @@
+vector<int> valueEqualToIndex(int arr[], int n) {
+	    vector<int> res;
+	    for(int i=0; i<n; i++){
+	        if(arr[i] == i+1){
+	            res.push_back(i+1);
+	        }
+	    }
+	    return res;
+	}
