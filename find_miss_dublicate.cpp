4594903c69985144f63fcd50297b54561eea7ec6
Rahul Kumar <33459576+RahulkumarRV@users.noreply.github.com>
2021-06-11 20:28:02 +0530

    Update find_miss_dublicate.cpp

--- find_miss_dublicate.cpp
+++ find_miss_dublicate.cpp
@@ -15,3 +15,33 @@ int *findTwoElement(int *arr, int n) {
         }
         return res;
     }
+
+
+int majorityElement(int a[], int size)
+    {
+        int maj_ele = 0, count = 1;
+        for(int i=1; i<size; i++){
+            if(a[maj_ele] == a[i]){
+                count++;
+            }else {
+                count--;
+            }
+            if(count == 0){
+                maj_ele = i;
+                count = 1;
+            }
+        }
+        int cand = a[maj_ele]; count = 0;
+        for(int i=0; i<size; i++){
+            if(a[i] == cand){
+                count++;
+            }
+        }
+        if(count > size / 2){
+            return cand;
+        }
+        else {
+            return -1;
+        }
+    }
+};
